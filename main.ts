2398feb4f0e456be9427de2db1631cbd6f3788c9
SadiaRazzaqJutt <sadiarazzaq14@gmail.com>
2024-07-07 12:13:15 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,22 @@
+
+import { log } from "console";
+import inquirer from "inquirer";
+
+const output = await inquirer.prompt([{message: "Enter 1st number", type: "number", name: "firstNumber"},
+    {message: "Enter 2nd number", type: "number", name: "secondNumber"},
+    {message: "Select operator", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+// Conditional Statement
+if (output.operator ==="Addition"){
+    console.log(output.firstNumber + output.secondNumber)
+} else if (output.operator ==="Subtraction"){
+    console.log(output.firstNumber - output.secondNumber)
+} else if (output.operator ==="Multiplication"){
+    console.log(output.firstNumber * output.secondNumber)
+} else if (output.operator ==="Division"){
+    console.log(output.firstNumber / output.secondNumber)
+} else {
+    console.log("Please select valid operator")
+}
